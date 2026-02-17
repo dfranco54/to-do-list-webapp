388f867d3cd496344e45eb4e3a9f64b6438706dd
Daniel Franco <131307734+dfranco54@users.noreply.github.com>
2026-02-16 19:35:53 -0500

    componentes y dependencias agregadas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <h1>AAAAAAAAAA</h1>
+    <div className='aplicacion-tareas'>
+      <div className='tareas-lista-principal'>
+        <h1>Lista de tareas</h1>
+        <ListaDeTareas />
+      </div>
     </div>
   );
 }
